93348f4cba17c03afd27977c750ea7a9d5cfc597
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-02 16:04:02 +0700

    Commit express exer

--- 08-nodejs/02-exercise/src/main.js
+++ 08-nodejs/02-exercise/src/main.js
@@ -2,3 +2,50 @@ import express from "express";
 const webServer = express();
 
 // code down below
+
+webServer.get('/', (req, res) => {
+    res.json("This is Root Route");
+});
+
+webServer.get('/news', (req, res) => {
+    res.json(
+        [
+            {
+                name: "news 1",
+                detail: "detail 1"
+            },
+            {
+                name: "news 2",
+                detail: "detail 2"
+            },
+            {
+                name: "news 3",
+                detail: "detail 3"
+            }
+        ]
+    );
+});
+
+webServer.get('/users', (req, res) => {
+    res.json(
+        [
+            { userId: "mock-id-1" },
+            { userId: "mock-id-2" },
+            { userId: "mock-id-3" },
+        ]
+    );
+});
+
+webServer.get('/me', (req, res) => {
+    res.json({
+        name: "mock-name",
+        role: "user",
+        picture: "https://shorturl.at/qtFJM"
+    });
+});
+
+webServer.get('/health-check', (req, res) => {
+    res.json("Server is fine!")
+});
+
+webServer.listen(3001);
--- 08-nodejs/04-exercise/src/main.js
+++ 08-nodejs/04-exercise/src/main.js
@@ -8,6 +8,27 @@ const port = 3001;
 // code here.
 const company = [];
 
+webServer.get('/', (req, res) => {
+  res.json('Hello World');
+});
+
+webServer.get('/company', (req, res) => {
+  res.json(company);
+});
+
+webServer.post('/company', (req, res) => {
+  const { name, taxId } = req.body;
+  const compId = company.length + 1;
+  const newCompany = {
+    companyId: "comp-" + compId,
+    name: name,
+    taxId: taxId,
+    employees: []
+  }
+  company.push(newCompany);
+  res.json('Create Company Success');
+});
+
 webServer.listen(port, ipAddress, () => {
   console.log(`Web Application Server is running on ${ipAddress} port ${port}`);
   console.log(`Address: http://${ipAddress}:${port}`);
